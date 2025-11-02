e2d492c688e3b26d40fc2994cf5fdd37c29470de
Peks <0peks0@proton.me>
2025-11-02 00:01:57 -0600

    feat(ui): add UserBadge for show name and avatar

---
+++ app/components/profile/UserBadge.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import UserIcon from '../ui/svgs/UserIcon';
+
+interface ProfileBadgeProps {
+  imageUrl?: string;
+  name: string;
+}
+
+export default function ProfileBadge({ imageUrl, name }: ProfileBadgeProps) {
+  function DefaultUserImage() {
+    return (
+      <figure className="h-(--icon-small-size) w-(--icon-small-size)">
+        <UserIcon />
+      </figure>
+    );
+  }
+  return (
+    <div className="flex flex-shrink-0 items-center gap-2">
+      {imageUrl ? (
+        <Image
+          src={imageUrl}
+          alt={`${name}'s avatar`}
+          width={32}
+          height={32}
+          className="rounded-full object-cover"
+        />
+      ) : (
+        <DefaultUserImage />
+      )}
+
+      <span className="text-foreground neon-effect font-heading text-4xl uppercase">
+        {name}
+      </span>
+    </div>
+  );
+}
