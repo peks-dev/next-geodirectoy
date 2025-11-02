48d107fece826c120742e5d1e8b4ba46c5dd5b0c
Peks <0peks0@proton.me>
2025-11-02 00:04:32 -0600

    feat(community): Open community location in device default map app
    - add OpenInMaps

---
+++ app/(main)/comunidad/components/OpenInMaps.tsx
@@ -0,0 +1,33 @@
+'use client';
+import Button from '@/app/components/ui/Button';
+import type { Coordinates } from '@/app/types/communityTypes';
+
+const OpenInMaps = ({ lat, lng }: Coordinates) => {
+  const handleClick = () => {
+    // URI para apps nativas de mapas
+    const iosURI = `maps://?q=${lat},${lng}`;
+    const androidURI = `geo:0,0?q=${lat},${lng}`;
+    const googleURI = `https://www.google.com/maps?q=${lat},${lng}`;
+
+    // Intenta abrir app nativa primero
+    if (/iPhone|iPad|iPod/.test(navigator.userAgent)) {
+      window.location.href = iosURI;
+    } else if (/android/i.test(navigator.userAgent)) {
+      window.location.href = androidURI;
+    } else {
+      window.open(googleURI, '_blank');
+    }
+  };
+
+  return (
+    <Button
+      onClick={handleClick}
+      variant="secondary"
+      className="bg-background-interactive absolute bottom-1 left-1 z-50"
+    >
+      Abrir en Mapas
+    </Button>
+  );
+};
+
+export default OpenInMaps;
