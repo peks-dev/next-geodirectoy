aa9cc47ae800f87543d2302115a80f51a1fecdff
Peks <0peks0@proton.me>
2025-11-06 16:07:44 -0600

    fix(communities): ServiceBadge better color contrast

--- app/components/ui/ServiceBadge.tsx
+++ app/components/ui/ServiceBadge.tsx
@@ -13,7 +13,7 @@ export default function ServiceItem({
 }: ServiceItemProps) {
   return (
     <li
-      className={`text-light-4 ${available ? 'neon-effect text-light-tertiary' : 'text-light-4'} flex items-center justify-center p-2 sm:p-4`}
+      className={`text-foreground ${available ? 'neon-effect' : 'opacity-30'} flex items-center justify-center p-2 sm:p-4`}
       aria-label={available ? `${label} disponible` : `${label} no disponible`}
       title={available ? `${label} disponible` : `${label} no disponible`}
     >
@@ -23,9 +23,7 @@ export default function ServiceItem({
         </div>
         <div className="flex-1">
           <div className="flex">
-            <p
-              className={`font-heading text-center uppercase ${available ? 'text-light-tertiary' : 'text-light-4'} text-5x1`}
-            >
+            <p className={`font-heading text-5x1 text-center uppercase`}>
               {label}
             </p>
           </div>
