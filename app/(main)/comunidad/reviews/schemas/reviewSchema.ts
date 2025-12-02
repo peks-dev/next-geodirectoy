b356360444a93f0791157ac2066a7b0d7847f8ed
Peks <0peks0@proton.me>
2025-12-01 18:45:13 -0600

    refactor(reviews): ajust zod schema error messages

--- app/(main)/comunidad/reviews/schemas/reviewSchema.ts
+++ app/(main)/comunidad/reviews/schemas/reviewSchema.ts
@@ -1,7 +1,13 @@
 import * as z from 'zod';
 
 export const reviewForm = z.object({
-  rating: z.number().min(1).max(5, 'La valoración debe estar entre 1 al 5'),
-  comment: z.string().min(8).max(250, 'superaste el limite de 250 caracteres'),
+  rating: z
+    .number()
+    .min(1, 'debes de seleccionar un puntaje')
+    .max(5, 'no puede darle más de 5'),
+  comment: z
+    .string()
+    .min(8, 'debe ser mas largo el comentario')
+    .max(250, 'el comentario es muy largo'),
   community_id: z.string().min(10).max(36),
 });
